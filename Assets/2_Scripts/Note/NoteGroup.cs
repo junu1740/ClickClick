22d9a479834a23829f993b346dc5e838f22b3796
kimjunWoo <163072073+junu1740@users.noreply.github.com>
2024-03-28 11:00:43 +0900

    Update NoteGroup.cs

--- Assets/2_Scripts/Note/NoteGroup.cs
+++ Assets/2_Scripts/Note/NoteGroup.cs
@@ -7,10 +7,17 @@ public class NoteGroup : MonoBehaviour
 {
     [SerializeField] private int noteMaxMum = 5;
     [SerializeField] private GameObject notePrefab;
+
+    private List<Note> noteList = new List<Note>();
     void Start()
     {
-        GameObject noteGameObj = Instantiate(notePrefab);
-        Note note = noteGameObj.GetComponent<Note>();
+        for(int i = 0; i < noteMaxMum; i++)
+        {
+            GameObject noteGameObj = Instantiate(notePrefab);
+            Note note = noteGameObj.GetComponent<Note>();
+            noteList.Add(note);
+        }
+       
         
     }
 
