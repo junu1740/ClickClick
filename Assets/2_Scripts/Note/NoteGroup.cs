97594b32680715aa8e74765c99c09462a76c142b
kimjunWoo <163072073+junu1740@users.noreply.github.com>
2024-03-28 12:39:17 +0900

    과일과 버튼 오브젝트 투입 과일 여러개 생성 버튼 애니메이션 추가

--- Assets/2_Scripts/Note/NoteGroup.cs
+++ Assets/2_Scripts/Note/NoteGroup.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 
 public class NoteGroup : MonoBehaviour
 {
-    [SerializeField] private int noteMaxMum = 5;
+    [SerializeField] private int noteMaxNum = 5;
     [SerializeField] private GameObject notePrefab;
+    [SerializeField] private GameObject noteSpawn;
+    [SerializeField] private float noteGap = 6f;
 
     private List<Note> noteList = new List<Note>();
     void Start()
     {
-        for(int i = 0; i < noteMaxMum; i++)
+        for(int i = 0; i < noteMaxNum; i++)
         {
             GameObject noteGameObj = Instantiate(notePrefab);
+            noteGameObj.transform.SetParent(noteSpawn.transform);
+
+            noteGameObj.transform.localPosition = Vector3.up * this.noteList.Count * noteGap;
             Note note = noteGameObj.GetComponent<Note>();
             noteList.Add(note);
         }
