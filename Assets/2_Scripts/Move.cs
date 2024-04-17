396cf8aa892db0b65acf0c76967bb05b8a373eca
kimjunWoo <163072073+junu1740@users.noreply.github.com>
2024-04-17 15:40:14 +0900

    애니메이션 추가

---
+++ Assets/2_Scripts/Move.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    Animator anim;
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I)) 
+        {
+            anim.CrossFade("New Animation", 0, 0);
+        } 
+
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.CrossFade("Attack", 0, 0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.CrossFade("Death", 0, 0, 0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.CrossFade("Walk", 0, 0);
+        }
+
+        
+        
+    }
+}
